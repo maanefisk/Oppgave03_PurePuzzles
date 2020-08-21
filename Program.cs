0be7edd46bfcf8b81eba6be729a3a7bd9f4a3386
maanefisk <62015771+maanefisk@users.noreply.github.com>
2020-08-21 18:55:38 +0200

    NOw its a square, gonna be a pyramid.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 
 namespace Oppgave03_PurePuzzles
 {
@@ -6,7 +7,21 @@ namespace Oppgave03_PurePuzzles
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int b = 0; b < 4; b++)
+            { 
+                for (int a = 0; a < 8; a++) 
+                {
+                            Console.Write("#");
+                }
+                for (int c = 0; c < 8; c++)
+                {
+                    Console.Write("S");
+                }
+                Console.WriteLine("M");
+            }
+
+            
+            
         }
     }
 }
